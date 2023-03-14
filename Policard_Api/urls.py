52627aeac83392a41e845ebe54e487f86cce4ee1
Cesar23Zepeda <cesar.zepeda@igrtec.com>
2023-03-14 14:24:19 -0600

    Render

--- Policard_Api/urls.py
+++ Policard_Api/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
 
     path('api/', include('apps.users.urls'), name='users'),
     
-    path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path(r'redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
