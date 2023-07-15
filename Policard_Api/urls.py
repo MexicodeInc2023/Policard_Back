29ead68a97dbafbb0358c18fbb240ec38228be04
CesarZepeda23 <cz34101@hotmail.com>
2023-07-14 18:46:57 -0600

    Policard Render v2

--- Policard_Api/urls.py
+++ Policard_Api/urls.py
@@ -37,6 +37,6 @@ urlpatterns = [
 
     path('api/', include('apps.users.urls'), name='users'),
     
-    path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
