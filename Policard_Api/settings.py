3d3ea4f8b8b9c1d28d5daf5826298519e46850ab
CesarZepeda23 <cz34101@hotmail.com>
2023-07-19 19:25:17 -0600

    Render Img

--- Policard_Api/settings.py
+++ Policard_Api/settings.py
@@ -132,15 +132,25 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(dj_database_url, "media")
 MEDIA_URL = "media/"
 
+STATIC_ROOT = os.path.join('dj_database_url', 'staticfiles')
 STATIC_URL = 'static/'
-if not DEBUG:  
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join('dj_database_url', 'static'),
+)
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/4.0/howto/static-files/
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "media/"
+
+# STATIC_URL = 'static/'
+# if not DEBUG:  
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
